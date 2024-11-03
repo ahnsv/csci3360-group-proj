f9c645ea913d8d528cdcc676ba4c85e40a612de5
ahnsv <ahnsv@bc.edu>
2024-11-03 12:37:31 -0500

    fix: refer usecase

--- backend/src/router/task.py
+++ backend/src/router/task.py
@@ -3,17 +3,18 @@ from fastapi import APIRouter
 from src.database.models import Task
 from src.deps import AsyncDBSession, CurrentUser
 from src.schema import TaskIn
+from src.application import usecase_v2
 
 router = APIRouter(prefix="/task", tags=["task"])
 
 @router.post("/", response_model=Task)
 async def create_task(request: TaskIn, current_user: CurrentUser, session: AsyncDBSession):
-    return await create_task(session, current_user.id, request)
+    return await usecase_v2.create_task(session, current_user.id, request)
 
 @router.get("/", response_model=list[Task])
 async def list_tasks(current_user: CurrentUser, session: AsyncDBSession):
-    return await list_tasks(session, current_user.id)
+    return await usecase_v2.list_tasks(session, current_user.id)
 
 @router.get("/{task_id}", response_model=Task)
 async def get_task(task_id: int, current_user: CurrentUser, session: AsyncDBSession):
-    return await get_task(session, current_user.id, task_id)
+    return await usecase_v2.get_task(session, current_user.id, task_id)

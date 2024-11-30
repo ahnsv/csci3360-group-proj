059af08e1a710b5f39461ab51ef1317279d56685
ahnsv <ahnsv@bc.edu>
2024-11-30 15:01:04 -0500

    fix: build errors

--- frontend/src/components/chatroom/chat-scheduler.tsx
+++ frontend/src/components/chatroom/chat-scheduler.tsx
@@ -3,11 +3,11 @@
 import { ResizablePanel, ResizablePanelGroup } from "../ui/resizable";
 import ChatArea from './chat-area';
 
-export default function ChatScheduler({accessToken}: {accessToken: string}) {
+export default function ChatScheduler() {
     return (
         <ResizablePanelGroup direction="horizontal" className="flex h-full bg-gray-100 text-sm tracking-tight">
             <ResizablePanel className="w-1/2">
-                <ChatArea accessToken={accessToken} />
+                <ChatArea chatroomId={null} />
             </ResizablePanel>
         </ResizablePanelGroup>
     )
